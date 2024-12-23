bace6a89f8450cabe41c1ba19f42927ae90b8a6b
Ryan Coughlin <ryan@ryancoughlin.com>
2024-12-23 15:33:37 -0500

    Initial Commit

---
+++ Snake/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Snake
+//
+//  Created by Ryan Coughlin on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
